fd1325c7f651904bfdb47d8b3af28d01d2533a39
Documenter.jl <documenter@juliadocs.github.io>
2024-03-13 08:00:15 +0000

    build based on 283e82a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GoldbergerWiseRadionSpectra","category":"page"},{"location":"#GoldbergerWiseRadionSpectra","page":"Home","title":"GoldbergerWiseRadionSpectra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GoldbergerWiseRadionSpectra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GoldbergerWiseRadionSpectra]","category":"page"}]
+}
