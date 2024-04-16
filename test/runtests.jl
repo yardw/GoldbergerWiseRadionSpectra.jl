1386514c8b41bc293896bbfdb5b2924ee6c981eb
OUTKon <siegoutk@outlook.com>
2024-04-16 23:19:00 +0800

    Add Julia environment path to VSCode settings and update test code

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using GoldbergerWiseRadionSpectra
 using Test
 
 @testset "GoldbergerWiseRadionSpectra.jl" begin
-    m2s_analytical
+    @test m2s_analytical(1e-3, 1e3) isa Number
+    @test isapprox(search(1e-3, 1e3)[1], m2s_analytical(1e-3, 1e3), rtol=1e-1)
 end
