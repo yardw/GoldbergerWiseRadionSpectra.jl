b4ee924eb29915cf764c98eb32f305f384fccb0f
OUTKon <siegoutk@outlook.com>
2024-04-16 23:46:11 +0800

    Refactor test code in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,9 @@ using GoldbergerWiseRadionSpectra
 using Test
 
 @testset "GoldbergerWiseRadionSpectra.jl" begin
-    @test m2s_analytical(1e-3, 1e3) isa Number
-    @test isapprox(search(1e-3, 1e3)[1], m2s_analytical(1e-3, 1e3), rtol=1e-1)
+    l2 = 1e-3
+    g2 = 1e3
+    @test m2s_analytical(l2, g2) isa Number
+    @test isapprox(search(l2, g2)[1], m2s_analytical(l2, g2), rtol=1e-1)
+    @test extract_sol([l2, g2, search(l2, g2)...]) isa Function
 end
